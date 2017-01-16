71348dabf510599860c1a473ddee9c4f2f957cf6
Cho <minscho@ebay.com>
2017-01-16 15:09:19 +0900

    correnct typo

--- src/main/java/org/openshift/InsultResource.java
+++ src/main/java/org/openshift/InsultResource.java
@@ -13,7 +13,7 @@ public class InsultResource {
 	@Produces("application/json")
 	public HashMap<String,String> getInsult() {
 		HashMap<String,String> theInsult = new HashMap<String,String>();
-		theInsput.put("insult", new InsultGenerator().generateInsult());
+		theInsult.put("insult", new InsultGenerator().generateInsult());
 		return theInsult;
 	}
 }
